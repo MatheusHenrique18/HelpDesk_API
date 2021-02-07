332bc2299fabae0acbbf2e7372a44e089d1f5b03
MatheusHenrique18 <matheus.hsb18@gmail.com>
2021-02-07 19:01:59 -0300

    subindo ajustes relacionados a mudanca de status do chamado, inserindo regra de controle de acesso.

--- src/main/java/com/mhsb/helpdesk/controller/ChamadoController.java
+++ src/main/java/com/mhsb/helpdesk/controller/ChamadoController.java
@@ -15,12 +15,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mhsb.helpdesk.dto.ContadoresChamado;
 import com.mhsb.helpdesk.entity.Chamado;
@@ -33,6 +36,9 @@ import com.mhsb.helpdesk.security.jwt.JwtTokenUtil;
 import com.mhsb.helpdesk.service.ChamadoService;
 import com.mhsb.helpdesk.service.UsuarioService;
 
+@RestController
+@RequestMapping("/api/chamado")
+@CrossOrigin( origins = "*")
 public class ChamadoController {
 
 	@Autowired
@@ -187,13 +193,13 @@ public class ChamadoController {
 		return ResponseEntity.ok(response);
 	}
 	
-	@GetMapping(value = "{page}/{count}/{numero}/{titulo}/{status}/{prioridade}/{assinado}")
+	@GetMapping(value = "{page}/{count}/{numero}/{titulo}/{status}/{prioridade}") //	/{assinado}
 	@PreAuthorize("hasAnyRole('CLIENTE','TECNICO')")
 	public ResponseEntity<Response<Page<Chamado>>> findByParams (HttpServletRequest request,
 			@PathVariable("page") int page, @PathVariable("count") int count,
 			@PathVariable("numero") Integer numero, @PathVariable("titulo") String titulo,
-			@PathVariable("status") String status, @PathVariable("prioridade") String prioridade,
-			@PathVariable("assinado") boolean assinado){
+			@PathVariable("status") String status, @PathVariable("prioridade") String prioridade
+			/*@PathVariable("assinado") boolean assinado*/){
 		
 		titulo = titulo.equals("uninformed") ? "" : titulo;
 		status = status.equals("uninformed") ? "" : status;
@@ -207,11 +213,11 @@ public class ChamadoController {
 		}else {
 			Usuario usuarioRequest = usuarioFromRequest(request);
 			if(usuarioRequest.getPerfil().equals(PerfilEnum.ROLE_TECNICO)) {
-				if(assinado) {
-					chamados = chamadoService.findByParameterAndAssignedUser(page, count, titulo, status, prioridade, usuarioRequest.getId());
-				}else {
+//				if(assinado) {
+//					chamados = chamadoService.findByParameterAndAssignedUser(page, count, titulo, status, prioridade, usuarioRequest.getId());
+//				}else {
 					chamados = chamadoService.findByParameters(page, count, titulo, status, prioridade);
-				}
+//				}
 			} else if(usuarioRequest.getPerfil().equals(PerfilEnum.ROLE_CLIENTE)) {
 				chamados = chamadoService.findByParametersAndCurrentUser(page, count, titulo, status, prioridade, usuarioRequest.getId());
 			}
@@ -243,7 +249,14 @@ public class ChamadoController {
 			chamadoAtual.setStatus(StatusEnum.getStatus(status));
 			
 			if(status.equals("DESIGNADO")) {
-				chamadoAtual.setUsuarioDesignado(usuarioFromRequest(request));
+				Usuario usuarioAtual = usuarioFromRequest(request);
+				
+				if(usuarioAtual.getPerfil().equals(PerfilEnum.ROLE_CLIENTE)) {
+					response.getErros().add("Usuário com perfil de CLIENTE não pode designar chamados.");
+					return ResponseEntity.badRequest().body(response);
+				}else {
+					chamadoAtual.setUsuarioDesignado(usuarioAtual);
+				}
 			}
 			
 			Chamado chamadoPersistido = (Chamado) chamadoService.createOrUpdate(chamadoAtual);
